422e89071f656f6cb0ba8a8bc9a81798b63d5346
fabiog <fabio.gariglio@thetrainline.com>
2017-11-01 10:39:22 +0000

    add react.js and babel

---
+++ _wwwroot/src/app.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return (<h1>Fabio Gariglio</h1>);
+}
+
+ReactDOM.render(<App />, document.getElementById("body"));
---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return React.createElement(
+    "h1",
+    null,
+    "Fabio Gariglio"
+  );
+};
+
+ReactDOM.render(React.createElement(App, null), document.getElementById("body"));
