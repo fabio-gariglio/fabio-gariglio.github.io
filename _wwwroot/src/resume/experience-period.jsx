a9fe75c1b51aeb8d10d8b9d76752a70e49d09592
fabiog <fabio.gariglio@thetrainline.com>
2017-11-09 13:59:12 +0000

    Stringify experience period

--- _wwwroot/src/resume/experience-period.jsx
+++ _wwwroot/src/resume/experience-period.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import moment from 'moment';
 
 import styles from './style.css';
 
 module.exports = function ExperiencePeriod (props) {
+
+    function stringifyDate(value) {
+
+        if (!value) return 'Now';
+
+        var date = moment(new Date(value));
+        return date.format('MMM YYYY');
+    }
+
+    var dateFrom = stringifyDate(props.from);
+    var dateTo = stringifyDate(props.to);
+
     return (
-        <p className={styles.experienceDate}>Apr 2017 - Now</p>
+        <p className={styles.experienceDate}>{dateFrom} - {dateTo}</p>
     );
 }
